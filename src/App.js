5832b57393a580636174d8e28cca0c925be415e2
m0loch <romeo.graifenberg@gmail.com>
2022-03-26 14:21:47 +0100

    react-router test - in order to check GitHub Pages behaviour

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Main from './components/main';
+import About from './components/about';
+
 
 function App() {
+
   return (
     <div className="App">
+      <Routes>
+
+        <Route path="/about" element={<About/>} />
+        <Route path="/homepage" element={<Main/>} />
+
+      </Routes>
+
+      <a href="mailto:romeo.graifenberg@gmail.com">Contact me</a>
     </div>
   );
 }
