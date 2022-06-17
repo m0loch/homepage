b0c9bf11c8bacd7a848c35971a48016aaf5cfd34
m0loch <romeo.graifenberg@gmail.com>
2022-06-17 23:05:08 +0200

    Wordle republishing

--- src/data/posts.js
+++ src/data/posts.js
@@ -122,6 +122,23 @@ const posts = [{
     ),
     signature: "Romeo"
 },
+{
+    key: 8,
+    title: "Wordle is back!",
+    date: new Date("2022-06-17"),
+    content: `
+    ... I mean, don't get me wrong - I know thet the Wordle hype out there is not what it was some months ago, yet after all this time I managed to reupload my revisioned version and finally I can sleep peacefully.
+
+    The fun thing about it is that I've finally found the strength to go back to that in this period of massive work stress, with funny, moving deadlines and colleagues that either f* things up in the wrong moment or vanish into thin air without any apparent reason.
+
+    But no worries, things could be worse, like that time I took down the production database after a simple misclick.
+    Like, a couple of days ago?
+
+    We're living some fun times, I've got to say.
+    `,
+    signature: `In serious need of some vacation
+    Romeo`
+}
 ];
 
 export default posts;
