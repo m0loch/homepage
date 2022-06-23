0f0fd4988c464f7c007c50b0e143b3a2a77ca329
m0loch <romeo.graifenberg@gmail.com>
2022-06-23 14:24:25 +0200

    New post

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,4 +1,5 @@
 import charmeo from '../images/meo.gif';
+import { Link } from "react-router-dom";
 
 const posts = [{
     key: 0,
@@ -138,7 +139,22 @@ const posts = [{
     `,
     signature: `In serious need of some vacation
     Romeo`
-}
+},
+{
+    key: 9,
+    title: "Vacations, vacations",
+    date: new Date("2022-06-23"),
+    smartContent: (
+        <>
+            <p> After the release of https://ddrop.it/ and the relative crunch, I've finally been able to take some time off work and come to <a href="https://en.wikipedia.org/wiki/Genoa" target="_blank">Genoa</a> and meet my friends.</p>
+            <br />
+            <p>    We're planning to work a little on BugFormer in the next days but also to spend some time at the beach -which is something I don't get to do very often while living on the alps.</p>
+            <br />
+            <p>           I've took advantage of this sudden free time and finally fixed the last two things that bothered me on Wordle, now I'd like to put some work on the <Link to="/projects">projects section</Link> and then maybe port a game that me and <a href="https://www.linkedin.com/in/davidejones88">Dav</a> released for iOS nearly a decade ago.</p>
+        </>
+    ),
+    signature: "Romeo"
+},
 ];
 
 export default posts;
