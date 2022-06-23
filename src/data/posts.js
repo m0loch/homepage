69aad5681b6562a9357d30b729b34323dd6c869f
m0loch <romeo.graifenberg@gmail.com>
2022-06-23 14:25:26 +0200

    Fixed a warning

--- src/data/posts.js
+++ src/data/posts.js
@@ -146,11 +146,11 @@ const posts = [{
     date: new Date("2022-06-23"),
     smartContent: (
         <>
-            <p> After the release of https://ddrop.it/ and the relative crunch, I've finally been able to take some time off work and come to <a href="https://en.wikipedia.org/wiki/Genoa" target="_blank">Genoa</a> and meet my friends.</p>
+            <p> After the release of https://ddrop.it/ and the relative crunch, I've finally been able to take some time off work and come to <a href="https://en.wikipedia.org/wiki/Genoa" target="_blank" rel="noreferrer">Genoa</a> and meet my friends.</p>
             <br />
-            <p>    We're planning to work a little on BugFormer in the next days but also to spend some time at the beach -which is something I don't get to do very often while living on the alps.</p>
+            <p>We're planning to work a little on BugFormer in the next days but also to spend some time at the beach -which is something I don't get to do very often while living on the alps.</p>
             <br />
-            <p>           I've took advantage of this sudden free time and finally fixed the last two things that bothered me on Wordle, now I'd like to put some work on the <Link to="/projects">projects section</Link> and then maybe port a game that me and <a href="https://www.linkedin.com/in/davidejones88">Dav</a> released for iOS nearly a decade ago.</p>
+            <p>I've took advantage of this sudden free time and finally fixed the last two things that bothered me on Wordle, now I'd like to put some work on the <Link to="/projects">projects section</Link> and then maybe port a game that me and <a href="https://www.linkedin.com/in/davidejones88">Dav</a> released for iOS nearly a decade ago.</p>
         </>
     ),
     signature: "Romeo"
