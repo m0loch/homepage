4924ffae5334dd1ad3147f5bde000af6a0abdb75
m0loch <romeo.graifenberg@gmail.com>
2022-07-10 00:31:32 +0200

    Fixed typo!

--- src/components/projectsSection/futurePlans.js
+++ src/components/projectsSection/futurePlans.js
@@ -6,7 +6,7 @@ export default function FuturePlans() {
             <p>First thing first, I'm not 100% happy with how the projects section works at the moment.</p>
             <p>That'll be a work in progress for a while, I guess</p>
             <br/>
-            <p>With that (at least partially out of the way, the next step would be to add some settings for each game, in order to have the fifteen's puzzle actually be a 3x3 puzzle or letting the user select mosaic's basic picture for example.</p>
+            <p>With that (at least partially) out of the way, the next step would be to add some settings for each game, in order to have the fifteen's puzzle actually be a 3x3 puzzle or letting the user select mosaic's basic picture for example.</p>
             <br/>
             <p>Adding settings, though, will force me to think about handling state/cookies and, <i>potentially</i>, adding a cookie disclaimer inside the homepage.</p>
             <br/>
