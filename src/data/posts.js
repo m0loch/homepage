ef58e7f9062d6ffb4b2b8e7d27874a57131f04b1
m0loch <romeo.graifenberg@gmail.com>
2022-07-10 09:32:26 +0200

    New post (added redux)

--- src/data/posts.js
+++ src/data/posts.js
@@ -169,6 +169,22 @@ const posts = [{
     `,
     signature: "Romeo"
 },
+{
+    key: 11,
+    title: "Settings",
+    date: new Date("2022-07-10"),
+    content: `
+    I wanted to expand a little on the existing projects before starting to adding a new one, so now it's possible to play the Fifteen puzzle on a 3x3 board (which makes it not a 15 puzzle anymore, but I digress).
+
+    In order to add the settings and make them work, though, I ended up having to import Redux in the project - and that's something I wanted to avoid, in order to try to keep the project <i>easy and relaxed</i>.
+
+    Installing the redux and react-redux packages, as one can imagine, proved to be an easy task: you just need to install them using your packet manager of choice, import them, realize that you can't access the site anymore because the SSL support has broken, update everything, <a href="https://github.com/akveo/react-native-ui-kitten/issues/996">fix yet another bug that breaks random things</a>, go on with your work.
+    
+    <center><h4>BOOOOOOORING~</h4></center>
+    `,
+    signature: `Rant over
+    Romeo`
+},
 ];
 
 export default posts;
