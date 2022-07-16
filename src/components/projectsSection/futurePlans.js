5fd63b1593728d6737f4b5dee76e081f0aee20a5
m0loch <romeo.graifenberg@gmail.com>
2022-07-16 14:47:32 +0200

    Updated FUTURE PLANS

--- src/components/projectsSection/futurePlans.js
+++ src/components/projectsSection/futurePlans.js
@@ -3,15 +3,6 @@ import React from 'react';
 export default function FuturePlans() {
     return (
         <>
-            <p>First thing first, I'm not 100% happy with how the projects section works at the moment.</p>
-            <p>That'll be a work in progress for a while, I guess</p>
-            <br/>
-            <p>With that (at least partially) out of the way, the next step would be to add some settings for each game, in order to have the fifteen's puzzle actually be a 3x3 puzzle or letting the user select mosaic's basic picture for example.</p>
-            <br/>
-            <p>Adding settings, though, will force me to think about handling state/cookies and, <i>potentially</i>, adding a cookie disclaimer inside the homepage.</p>
-            <br/>
-            <p>The other thing that needs to undergo some review is the win dialog - but that's far from being urgent, to be honest.</p>
-            <br/>
             <h4>Future games that I'd like to implement in the future (in no particular order)</h4>
             <ul>
                 <li>Tyles</li>
