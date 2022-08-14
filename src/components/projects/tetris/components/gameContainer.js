9fcd813894a365daf9395a45ad9426a6c4a00178
m0loch <romeo.graifenberg@gmail.com>
2022-08-15 00:33:16 +0200

    Added forgotten Tetris component...

---
+++ src/components/projects/tetris/components/gameContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { styled } from '@mui/system';
+import { Grid } from '@mui/material';
+
+const StyledGrid = styled(Grid)(
+    ({ theme }) => ({
+        width: "clamp(800px, 80% ,900px)",
+        margin: "auto",
+
+        [theme.breakpoints.down('md')]: {
+            width: "95%"
+        }
+    })
+);
+
+function GameContainer(props) {
+    return (
+        <StyledGrid container spacing={2} >
+            {props.children}
+        </StyledGrid>
+    );
+}
+
+export default GameContainer;
