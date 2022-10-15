5fe5adc2f27cec93125e0032d52af1abaefa3935
m0loch <romeo.graifenberg@gmail.com>
2022-10-15 14:58:12 +0200

    New post

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import charmeo from '../images/meo.gif';
+import profanities from '../images/profanities.jpeg';
 import { Link } from "react-router-dom";
 
 const posts = [{
@@ -242,6 +243,29 @@ const posts = [{
     Well, I thought I was fully prepared for the worst, but seeing the far-right win the italian elections really breaks my heart, that's why I'm ranting on my secret place on the internet at 3am instead of getting a good night of rest before yet another sad monday at work.
     `,
     signature: `:(`
+},
+{
+    key: 16,
+    title: "Rewrite everything",
+    date: new Date("2022-10-15"),
+    images: [profanities],
+    smartContent: (
+    <>
+        <p>I'll be straightforward: coding Tetris was fun back in the day when I had some fun with Tetris 99 and I discovered the existence of <a href="https://harddrop.com">harddrop.com</a> which gave me pleny of insight to write my own version.</p>
+        <br />
+        <p>I then proceeded writing my own stand-alone React version without even checking all the marks on the TODO list and moved on as soon as I found something more interesting.</p>
+        <br />
+        <p>Integrating that inside the website proved to be more annoying than it should because, having written it with a React.Component object oriented approach while the rest of the site was properly built on high order components, I found myself having to deal with some strange behaviours and interactions when it came down adding the mobile controls.</p>
+        <br />
+        <p>That lead to some good old debugging:</p>
+        <img alt="One day we'll have better integrated tweets, but today's not that day" src={profanities} style={{ display: "block", margin: "15px auto 0", maxWidth: "80%" }} />
+        <br />
+        <p>At the end of the day I basically started rewriting everything as an HOC and, to be truly honest, facing some interesting issues like the handling of the game loop via hooks, but that's taking an unreasonable amount of time that I don't really have at the moment.</p>
+        <br />
+        <p>And that's why I started Final Fantasy 7 Remake: Intergrade and preordered the remake of Crisis Core.</p>
+    </>
+    ),
+    signature: `Romeo`
 },
 ];
 
