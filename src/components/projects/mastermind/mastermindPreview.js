f8656aff06da02b644c82ff1bd63cb09b0f41833
m0loch <romeo.graifenberg@gmail.com>
2022-12-25 18:32:35 +0100

    MasterMind related post and preview

--- src/components/projects/mastermind/mastermindPreview.js
+++ src/components/projects/mastermind/mastermindPreview.js
@@ -25,7 +25,12 @@ function MastermindPreview(props) {
             />
             <Collapse in={open[0]} timeout="auto" unmountOnExit>
                 {splitText(`
-                TODO
+                    Master Mind (or Mastermind, or any combination whatsoever) is a simple game where two players face each other, one trying to come up with a difficult to break color pattern, the other having to guess it before runnig out of attempts.
+                    After each attempt the "code keeper" will give a feedback to the cracker about how their last effort performed, signalling them with a black dot for every correct digit and a white one for every colour that has been placed in the wrong spot.
+
+                    The number of attempts, digits and possible colours vary between versions and editions, and greatly impact the difficulty of the game itself.
+
+                    This version is obviously meant for single players, as the code will just handle generating the code and giving the appropriate feedback.
                 `)}
             </Collapse>
 
@@ -36,7 +41,12 @@ function MastermindPreview(props) {
             />
             <Collapse in={open[1]} timeout="auto" unmountOnExit>
                 {splitText(`
-                    TODO
+                    This minigame took way more time than intended, as I started out setting it up with PIXI.js then pivoted after realizing that I needed some kind of popup to select the orbs' colours.
+                    Initially I went for having them change colour every time they were tapped, but expecially on hard difficulties that proved to be quite annoying.
+
+                    After trying to add some dialogs that tried to resemble decently the win screen, and at a later point evaluating the idea of having multiple PIXI.js canvases, I just pivoted to removing PIXI.js from the project altogether.
+
+                    This version doesn't feature any asset, everything gets drawn and animated via CSS while the game logic is just plain JavaScript.
                 `)}
             </Collapse>
 
@@ -46,9 +56,6 @@ function MastermindPreview(props) {
                 onClick={() => handleStateChange(2)}
             />
             <Collapse in={open[2]} timeout="auto" unmountOnExit>
-                {splitText(`
-                    TODO
-                `)}
                 <RadioGroup
                     value={props.difficulty}
                     name="mode-select"
