b0c111470cbd87ca0052d2436d9deba0118d5c29
m0loch <romeo.graifenberg@gmail.com>
2022-12-25 22:12:02 +0100

    Fixed youtube embed

--- src/components/posts/youtubeVideo.js
+++ src/components/posts/youtubeVideo.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import { styled } from '@mui/system';
 
-const YouTubeVideo = ({ videoId }) =>
-    <div className="video-responsive">
-        <iframe
-            width="853"
-            height="480"
-            src={`https://www.youtube.com/embed/${videoId}`}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title="Embedded youtube"
-        />
-    </div>
+const DockArea = styled('div')({
+    position: "relative",
+    width: "100%",
+    height: 0,
+    paddingBottom: "56.25%",
+});
+
+const YTframe = styled('iframe')({
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+});
+
+const YouTubeVideo = ({ videoId }) => {
+    return (
+        <DockArea>
+            <YTframe
+                width="853"
+                height="480"
+                src={`https://www.youtube.com/embed/${videoId}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+            />
+        </DockArea>)
+}
 
 export default YouTubeVideo;
