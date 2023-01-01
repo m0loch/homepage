5ade92812efa6fa9e90ac7805fd4df46d2893a63
m0loch <romeo.graifenberg@gmail.com>
2023-01-01 23:00:50 +0100

    New year new post!

--- src/data/posts.js
+++ src/data/posts.js
@@ -350,7 +350,28 @@ const posts = [{
     ),
     signature: `HO HO HO
     Romeo`
-}
+},
+{
+    key: 21,
+    title: "2023 is here!",
+    date: new Date("2023-01-01"),
+    smartContent: (
+        <>
+            <p>Just a quick, mandatory post to wish everyone a splendid 2023!</p>
+            <br />
+            <p>I'm two weeks away from starting my new job experience and I'm really excited, even though I've yet to find a place to stay in Milan...</p>
+            <br />
+            <p>In the meanwhile I'm re-developing Tyles, a puzzle game me and my good friend <a href="https://www.linkedin.com/in/davidejones88/">Davide</a> published for iOS some 5 years ago - just to keep myself busy while struggling for the last two weeks of my old job (PrestaShop I solemnly swear I won't miss you). </p>
+            <br />
+            <p>The game engine and the levels are already ok, it will be published as soon as I fix some collateral code, like the win screen, the levels' handling etc</p>
+            <br />
+            <p>Keep yourselves entertained with my Top songs from 2022 according to Spotify:</p>
+            <br />
+            <iframe title="2022 in a nutshell" style={{ borderRadius: "20px" }} src="https://open.spotify.com/embed/playlist/4FFADuQuY0R6ufPfvEpYBp?utm_source=generator" width="100%" height="380" frameBorder="0"  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </>
+    ),
+    signature: `Romeo`
+},
 ];
 
 export default posts;
