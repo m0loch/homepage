186988ffae209b59277db63dad4d8d5eca3ab6b2
m0loch <romeo.graifenberg@gmail.com>
2023-01-07 15:50:58 +0100

    Updated future plans section

--- src/components/projectsSection/futurePlans.js
+++ src/components/projectsSection/futurePlans.js
@@ -3,11 +3,23 @@ import React from 'react';
 export default function FuturePlans() {
     return (
         <>
-            <h4>Future games that I'd like to implement in the future (in no particular order)</h4>
+            <h4>Coming up next:</h4>
+            <p>
+                There's some restructuring to do here and there on the whole website structure.
+                On one hand I want to refactor components in order to have a shared library across the whole site,
+                on the other I'm well aware I'm not an UI/UX designer by any means but I'd still like the site
+                to feel a little less "basic" and just a little more polished.
+                <br/>
+                I'm thinking I could aim for having 10 games available before starting the real refactor, I still have to think about it.
+            </p>
+
+            <h4>Future games that <i>I'd like</i> to implement in the future (in no particular order)</h4>
             <ul>
-                <li>Tyles (work in progress)</li>
                 <li>Minesweeper</li>
                 <li>Card solitaire</li>
+                <li>Sudoku</li>
+                <li>Snake</li>
+                <li>Jigsaw puzzle</li>
             </ul>
 
         </>
