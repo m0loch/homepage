1e9cce4a87b2c29be32e7052aaccf23397db059c
m0loch <romeo.graifenberg@gmail.com>
2023-02-20 23:33:36 +0100

    Post 24

--- src/data/posts.js
+++ src/data/posts.js
@@ -412,6 +412,23 @@ const posts = [{
     ),
     signature: 'Romeo'
 }
+,
+{
+    key: 24,
+    title: "Minor update",
+    date: new Date("2023-02-20"),
+    smartContent: (
+        <>
+            {splitText(`
+            Hi,
+            here's Meo
+            Tuning in just to say I've fixed the mobile version of Minesweeper.
+            `)}
+        </>
+    ),
+    signature: `Until next time
+    Romeo`
+}
 ];
 
 export default posts;
