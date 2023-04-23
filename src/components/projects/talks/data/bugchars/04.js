6ad162458bb6f707ae733433c5103ec495ea3cdf
m0loch <romeo.graifenberg@gmail.com>
2023-04-23 17:41:09 +0200

    Talks: question 4 and 5 for the Bugformer questionnaire

--- src/components/projects/talks/data/bugchars/04.js
+++ src/components/projects/talks/data/bugchars/04.js
@@ -3,12 +3,16 @@ import Shuffler from "../../shuffler";
 const question = {
     key: 'question04',
     content: `
+    As we all know, work is not everything.
+    Or at least that's what they say, I guess...
+
+    What's your take on that?
     `,
     choices: [
-        { text: "", outcome: { meo: 10, next: 'profile' } },
-        { text: "", outcome: { dav: 10, next: 'profile' } },
-        { text: "", outcome: { fra: 10, next: 'profile' } },
-        { text: "", outcome: { rob: 10, next: 'profile' } },
+        { text: "I would love to live in a world where people could lead some peaceful lives without the need of having jobs - but I don't, so I just try to find some job I like as I will end up spend a lot of time on that.", outcome: { meo: 10 } },
+        { text: "I actually enjoy working, if I find myself with too much free time on my hands I usually end up studying on planning some side hustles.", outcome: { dav: 10 } },
+        { text: "I work multiple jobs I hate in order to sustain myself and my hobbies, hoping one day to become an enterpreneur and finally work on something I like.", outcome: { fra: 10 } },
+        { text: "I've got plenty of hobbies, my job is just the one that earns me money for the other ones. I can't envision myself working on something I don't enjoy, I'd probably start looking around for a new one.", outcome: { rob: 10 } },
     ]
 }
 
--- src/components/projects/talks/data/bugchars/05.js
+++ src/components/projects/talks/data/bugchars/05.js
@@ -3,12 +3,17 @@ import Shuffler from "../../shuffler";
 const question = {
     key: 'question05',
     content: `
+    The pandemic is over and now you'll free to enjoy some fresh air and move around.
+
+    Or at least that's what your company told you when they said you need to start attending your office once again.
+
+    Being forced to restart commuting, what's your choice?
     `,
     choices: [
-        { text: "", outcome: { meo: 10, next: 'profile' } },
-        { text: "", outcome: { dav: 10, next: 'profile' } },
-        { text: "", outcome: { fra: 10, next: 'profile' } },
-        { text: "", outcome: { rob: 10, next: 'profile' } },
+        { text: "I usually move on foot or by bike, but I own a car for traveling longer distances.", outcome: { meo: 10 } },
+        { text: "I enjoy using public transportations on a daily basis, that keeps me from the stress of driving and lets me read books, listen to podcasts and, in a more general way, choose what to do with my free time.", outcome: { dav: 10 } },
+        { text: "It depends on various factors - I usually have to move by scooter / car on daily basis, but for longer travels I usually prefer taking a good old trains, especially when travelling alone.", outcome: { fra: 10 } },
+        { text: "I can't settle for having to conform to public transportations, I need to go to specific places when I feel like to rather than having to wait for buses, trains and the such. I'll just drive my car and be free.", outcome: { rob: 10 } },
     ]
 }
 
