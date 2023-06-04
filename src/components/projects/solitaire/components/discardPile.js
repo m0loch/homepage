2bed3242d9200beea0d7a5710508f95400169284
m0loch <romeo.graifenberg@gmail.com>
2023-06-04 17:24:47 +0200

    Solitaire: bug investigation

--- src/components/projects/solitaire/components/discardPile.js
+++ src/components/projects/solitaire/components/discardPile.js
@@ -8,6 +8,21 @@ class DiscardPile extends CardDocker {
         return removedCard;
     }
 
+    AddCard(card) {
+        let where = this.GetLandingPosition();
+
+        // DEBUG //
+        // const from = {x: card.x, y: card.y};
+        // console.log(`from (${from.x}, ${from.y}) to (${where.x}, ${where.y})`)
+        // /DEBUG //
+
+        card.x = where.x;
+        card.y = where.y;
+
+        this.cards.push(card);
+
+        this.addChild(card);
+    }
 }
 
 export default DiscardPile;
