52cc56b33d4d55f66be729f54ff52135d000a5ec
m0loch <romeo.graifenberg@gmail.com>
2023-06-22 20:02:45 +0200

    Minor refactor to runner.js

--- src/components/projects/runner/runner.js
+++ src/components/projects/runner/runner.js
@@ -7,8 +7,17 @@ function Runner(props) {
     const [textures, setTextures] = useState(undefined);
 
     useEffect(() => {
-        PIXI.Assets.add('char', `${props.assetsFolder}/nature-paltformer-tileset-16x16.json`, { scaleMode: PIXI.SCALE_MODES.NEAREST });
-        PIXI.Assets.add('tiles', `${props.assetsFolder}/Standard sprites upd.json`, { scaleMode: PIXI.SCALE_MODES.NEAREST });
+        PIXI.Assets.add(
+            'char',
+            `${props.assetsFolder}/nature-paltformer-tileset-16x16.json`,
+            { scaleMode: PIXI.SCALE_MODES.LINEAR }
+        );
+
+        PIXI.Assets.add('tiles',
+            `${props.assetsFolder}/Standard sprites upd.json`,
+            { scaleMode: PIXI.SCALE_MODES.LINEAR }
+        );
+
         PIXI.Assets.load(['char', 'tiles'])
             .then(textures => {
                 setTextures(textures);
@@ -18,7 +27,15 @@ function Runner(props) {
     }, [props.assetsFolder]);
 
     return (
-        <div style={{ position: "relative", margin: "auto", width: "100%", height: "100%", display: "flex", alignContent: "center", justifyContent: "center" }}>
+        <div style={{
+            position: "relative",
+            margin: "auto",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignContent: "center",
+            justifyContent: "center"
+        }}>
             <Stage
                 style={{ margin: "auto" }}
                 options={{
