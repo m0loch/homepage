4930f4a1d6198ee1abc23700e2645622366aa1a9
Romeo Graifenberg <>
2024-02-17 10:28:48 +0100

    Extra posts that were sent directly to publishing

---
+++ src/data/posts/0036.js
@@ -0,0 +1,22 @@
+const post = {
+    title: "Work test",
+    date: new Date("2023-12-05"),
+    smartContent: 
+    <>
+        <h1>Deep link 4 test:</h1>
+        <a href="sfc://sisal.it?referral=IAMMEO">REFERRAL (IAMMEO)</a><br/>
+        <br/>
+        <a href="sfc://sisal.it?goto=store">SHOP</a><br/>
+        <a href="sfc://sisal.it?goto=leaderboard">LEADERBOARD</a><br/>
+        <a href="sfc://sisal.it?goto=quests">QUESTS</a><br/>
+        <a href="sfc://sisal.it?goto=prizes">PRIZES</a><br/>
+        <a href="sfc://sisal.it?goto=minigames">MINIGAMES</a><br/>
+        <a href="sfc://sisal.it?goto=quiz">QUIZ</a><br/>
+        <a href="sfc://sisal.it?goto=pvp">PVP</a><br/>
+        <a href="sfc://sisal.it?goto=luckyspin">LUCKY SPIN</a>
+    </>,
+    signature: `One day I'll post something new, I swear
+    Romeo`
+};
+
+export default post;
---
+++ src/data/posts/0037.js
@@ -0,0 +1,11 @@
+const post = {
+    title: "Happy 2024!",
+    date: new Date("2024-01-11"),
+    content: `
+    I wanted to start 2024 with a big new post filled to the brim with updates!
+    ...but I don't really have anything to add at the moment, sorry ^_^;
+    `,
+    signature: `Romeo`
+};
+
+export default post;
