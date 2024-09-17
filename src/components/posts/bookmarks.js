c9d684b1d77a01ec2fa53d25380800292178122e
m0loch <romeo.graifenberg@gmail.com>
2024-09-17 08:53:07 +0200

    Custom bookmark support for closed sections - not the best solution, but it kind of works

--- src/components/posts/bookmarks.js
+++ src/components/posts/bookmarks.js
@@ -1,15 +1,44 @@
 const GoToContent = (e, label) => {
     e.preventDefault();
+    console.log("LET'S GOOOO")
     document.querySelector(`#${label}`)?.scrollIntoView({
         behavior: "smooth"
     });
 }
 
 export function BookmarkLink(props) {
+
+    function checkBookmark(e, label, isFirst) {
+        if (!document.querySelector(`#${label}`))
+        {
+            setTimeout(() => { checkBookmark(e, props.id, false) }, 100);            
+        }
+        else
+        {
+            if (isFirst) {
+                GoToContent(e, props.id);
+            }
+            else {
+                setTimeout(() => { GoToContent(e, props.id) }, 2000);
+            }
+        }
+    }
+
     return (
         <a
             href={`#${props.id}`}
-            onClick={e => GoToContent(e, props.id)}
+            onClick={e => {
+                if (props.onPreClick) 
+                {
+                    e.preventDefault();
+                    props.onPreClick();
+                    checkBookmark(e, props.id, true);
+                }
+                else
+                {
+                    GoToContent(e, props.id)
+                }
+            }}
             style={{...props.style, ...(props.isTitle ? {fontSize: '1.17rem', fontWeight: "bold"} : null)}}
         >
             {props.children}
