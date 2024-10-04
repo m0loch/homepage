6fbfe0a5c12a5c4b2466cee141e6ed71e67d6125
m0loch <romeo.graifenberg@gmail.com>
2024-10-04 21:41:39 +0200

    Added align property for YouTubeVideo components

--- src/components/posts/youtubeVideo.js
+++ src/components/posts/youtubeVideo.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { styled } from '@mui/system';
 
+const FullContainer =  styled('div')(({ align }) => {
+    return ({
+        display: "flex",
+        flexDirection: "column",
+        float: align,
+        marginLeft: align === 'center' ? "auto" : "25px",
+        marginRight: align === 'center' ? "auto" : "25px",
+        width: `${align ? 50 : 100}%`
+    })
+});
+
 const DockArea = styled('div')({
     position: "relative",
     width: "100%",
     height: 0,
     paddingBottom: "56.25%",
+});
+
+const CaptionArea = styled('div')({
+    position: "relative",
+    width: "100%",
 });
 
 const YTframe = styled('iframe')({
@@ -16,22 +32,29 @@ const YTframe = styled('iframe')({
     height: "100%",
 });
 
-const YouTubeVideo = ({ videoId, caption }) => {
+const YouTubeVideo = ({ videoId, caption, align }) => {
     return (
-        <>
-        <DockArea>
-            <YTframe
-                width="853"
-                height="480"
-                src={`https://www.youtube.com/embed/${videoId}`}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="Embedded youtube"
-            />
-        </DockArea>
-        {caption ? <center><i>{caption}</i><br/></center> : null}
-        </>
+        <FullContainer align={align}>
+            <DockArea>
+                <YTframe
+                    width="853"
+                    height="480"
+                    src={`https://www.youtube.com/embed/${videoId}`}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title={caption ?? "Embedded video from YouTube"}
+                />
+            </DockArea>
+            {caption ? 
+                <CaptionArea align={align}>
+                    <center>
+                        <i>{caption}</i>
+                        <br/>
+                    </center>
+                </CaptionArea>
+            : null}
+        </FullContainer>
     )
 }
 
