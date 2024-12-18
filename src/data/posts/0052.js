671e7e90f3c6cb38b13aabe6371c41d479401932
m0loch <romeo.graifenberg@gmail.com>
2024-12-19 00:13:05 +0100

    Post 52: added sudoku

---
+++ src/data/posts/0052.js
@@ -0,0 +1,20 @@
+import { splitText } from '../../components/common/textFunctions';
+
+const post = {
+    title: "Sudoku time!",
+    date: new Date("2024-12-19"),
+    smartContent: (
+        <>
+            {splitText(`
+            Lately I ended up doing some QA on a sudoku minigame developed by a third party, so I just decided to make one of my own because I was kind of mad at being in charge of a lot of collateral, not-so-funny work while such things get developed by external contractors.
+
+            But I think that this is the price of getting old and thus being engulfed in the more functional aspect of the job.
+
+            Well, enjoy 200 free sudokus if you will.
+            `)}
+        </>
+    ),
+    signature: `Romeo`
+};
+
+export default post;
