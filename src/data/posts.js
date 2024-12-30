eb94ed9dfd2749a9c52c26d8dff75594aa6e256a
m0loch <romeo.graifenberg@gmail.com>
2024-12-30 16:38:17 +0100

    Fixed bug on posts loading - oldest posts got broken as FF7 Rebirth subposts were loaded with the rest. Cutting the load from subfolders fixed the issue.

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,6 +1,6 @@
 function getPosts() {
     // Reads all the .js files in the ./posts subfolder as modules
-    const posts = require.context("./posts", true, /\.js$/);
+    const posts = require.context("./posts", false, /\.js$/);
 
     // Retrieves the default export for each module
     return posts.keys().map(post => posts(post).default);
