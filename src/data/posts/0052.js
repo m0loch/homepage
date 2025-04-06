93a61e403d50801ef9578d1316c994deb8ba819c
m0loch <romeo.graifenberg@gmail.com>
2025-04-06 04:38:49 +0200

    Post 0052: added direct link to the sudoku page

--- src/data/posts/0052.js
+++ src/data/posts/0052.js
@@ -1,4 +1,5 @@
 import { splitText } from '../../components/common/textFunctions';
+import { Link } from 'react-router-dom';
 
 const post = {
     title: "Sudoku time!",
@@ -9,9 +10,9 @@ const post = {
             Lately I ended up doing some QA on a sudoku minigame developed by a third party, so I just decided to make one of my own because I was kind of mad at being in charge of a lot of collateral, not-so-funny work while such things get developed by external contractors.
 
             But I think that this is the price of getting old and thus being engulfed in the more functional aspect of the job.
-
-            Well, enjoy 200 free sudokus if you will.
             `)}
+
+            <p>Well, <Link to="/projects/sudoku">enjoy 200 free sudokus if you will.</Link></p>
         </>
     ),
     signature: `Romeo`
