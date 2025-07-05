5972d70ff6bff3ee11ec1b039460be14d1bf070f
m0loch <romeo.graifenberg@gmail.com>
2025-07-05 20:41:09 +0200

    Updated preview for wherewolf

--- src/components/projects/wherewolf/wherewolfPreview.js
+++ src/components/projects/wherewolf/wherewolfPreview.js
@@ -5,7 +5,7 @@ import CollapseHeader from '../common/collapseHeader';
 import { splitText } from "../../common/textFunctions";
 
 function WerewolfPreview(props) {
-    const [open, setOpen] = useState([true, true, true]);
+    const [open, setOpen] = useState([true, true]);
 
     const handleStateChange = (idx) => {
         const newState = [...open];
@@ -23,34 +23,25 @@ function WerewolfPreview(props) {
             />
             <Collapse in={open[0]} timeout="auto" unmountOnExit>
                 {splitText(`
-                blah
+                Wherewolf is a <a href="https://en.wikipedia.org/wiki/Social_deduction_game" target="_blank" rel="noreferrer">social deduction game</a> where players take on the roles of villagers or werewolves.
+
+                There are many variants of the game since 1986 when Dimma Davidoff (Dimitri? Dmitriy? Let's just call him Дми́трий Давы́дов...) created <a href="https://www.servinglibrary.org/journal/2/the-original-mafia-rules" target="_blank" rel="noreferrer">the first version of Mafia</a>, from which many variants spawned over the years.
+
+                The one I'll be focusing on is the one developed by <a href="https://boardgamegeek.com/boardgamedesigner/6263/christian-zoli" target="_blank" rel="noreferrer">Christian Zoli</a> that was published in the early 2010s and that has sadly been discontinued.
+
+                This section is just a tool for the master to expedite the game setup during the summer parties I usually attend with my friends.
+
+                I believe that it could easily be rendered generic for other variants, but I wanted to focus on my personal use case.
                 `)}
-            </Collapse>
-
-            <CollapseHeader
-                caption={"Settings"}
-                open={open[1]}
-                onClick={() => handleStateChange(1)}
-            />
-            <Collapse in={open[0]} timeout="auto" unmountOnExit>
-            aaaa
-
             </Collapse>
 
             <CollapseHeader
                 caption={"Credits"}
-                open={open[2]}
+                open={open[1]}
                 onClick={() => handleStateChange(2)}
             />
             <Collapse in={open[2]} timeout="auto" unmountOnExit>
-                {splitText(`
-
-
-                The game is based on the original concept by Dimitri de Vries, who created the first version of the game in 1986. The game was later adapted by other developers and has since become a popular party game.
-                The game is also known as "Mafia" in some regions, and has been adapted into various formats, including board games and video games. The game has been played by millions of people worldwide and has become a staple of social gatherings and parties.
-
-                The variant this is based on blahblah vedi il manuale Christian Zoli
-                `)}
+                <a href="https://www.linkedin.com/in/christian-zoli/" target="_blank" rel="noreferrer">Christian Zoli</a> for the original game design.
             </Collapse>
         </div>
     );
