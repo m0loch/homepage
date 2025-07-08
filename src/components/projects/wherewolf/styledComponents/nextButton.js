1cd08ec9cb520a8661e3a103015314a5c4fe8827
m0loch <romeo.graifenberg@gmail.com>
2025-07-09 00:51:13 +0200

    NextButton non-functional refinement

--- src/components/projects/wherewolf/styledComponents/nextButton.js
+++ src/components/projects/wherewolf/styledComponents/nextButton.js
@@ -2,10 +2,9 @@ import { Button } from '@mui/material';
 import { styled } from '@mui/system';
 
 const NextButton = styled(Button)(
-    ({ theme }) => ({
+    () => ({
         margin: "8px auto",
         fontSize: "3vmin",
-
         width: "70%",
     })
 );
