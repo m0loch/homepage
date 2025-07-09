c1dfc45cd9014d0dd48e4d55567836a5692d2079
m0loch <romeo.graifenberg@gmail.com>
2025-07-09 07:32:14 +0200

    Changes to future plans component :)

--- src/components/projectsSection/futurePlans.js
+++ src/components/projectsSection/futurePlans.js
@@ -1,25 +1,19 @@
-import React from 'react';
+import { splitText } from '../../components/common/textFunctions';
 
 export default function FuturePlans() {
     return (
         <>
+            {splitText(`
             <h4>Coming up next:</h4>
             <p>
-                There's some restructuring to do here and there on the whole website structure.
-                On one hand I want to refactor components in order to have a shared library across the whole site,
-                on the other I'm well aware I'm not an UI/UX designer by any means but I'd still like the site
-                to feel a little less "basic" and just a little more polished.
-                <br/>
-                Now that I've reached the mileston of having 10 minigames available, I should start to really think about some real refactoring...
-            </p>
+            Well, well, well...
 
-            <h4>Future games that <i>I'd like</i> to implement in the future (in no particular order)</h4>
-            <ul>
-                <li>Sudoku</li>
-                <li>Snake</li>
-                <li>Jigsaw puzzle</li>
-            </ul>
+            Over time this pet project of mine has went from being a playground where I implemented some random games just for the sake of it, to what is basically a blog.
 
+            I don't know what I'll do next but I don't have much interest in adding minigames at the moment.
+
+            Sorry not sorry, I guess?
+            `)}
         </>
     );
 }
