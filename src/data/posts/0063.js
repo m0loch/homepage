13d018e23ab30fa2219c01813b1b1e6349841e93
m0loch <romeo.graifenberg@gmail.com>
2025-07-15 15:29:26 +0200

    Updated post 0063 with set description

--- src/data/posts/0063.js
+++ src/data/posts/0063.js
@@ -8,7 +8,8 @@ import { Collapse } from "@mui/material";
 
 import { ReactComponent as Logo } from '../../images/post-0063/ffxmtg.svg';
 
-import Chapter1 from "./0063/ff";
+import FFpage from "./0063/ff";
+import CustomImage from "../../components/posts/customImage";
 
 const StyledLogo = styled(Logo)(
     ({ theme }) => ({
@@ -58,6 +59,7 @@ function StatefulPost() {
                 onClick={() => handleStateChange(0)}
             />
             <StyledCollapse in={open[0]} timeout="auto" unmountOnExit>
+                <BookmarkLink id="Set description" isTitle>Set description</BookmarkLink>
                 <ol>
                     <BookmarkLink id="Final Fantasy" isTitle>Final Fantasy</BookmarkLink>
                     <li><BookmarkLink id="FF1Nutshell" onPreClick={() => forcePanelOpen(1)}>A bit of context</BookmarkLink></li>
@@ -67,6 +69,36 @@ function StatefulPost() {
             </StyledCollapse>
 
             <br/>
+            <Bookmark id="Set description" isTitle>Set description</Bookmark>
+            <CustomImage
+                src="https://cardkingdom.imgix.net/promotional-316038"
+                alt="Diabolos, Guardian Force - a Death's Shadow reprint"
+                align="right"
+            >
+                <i>
+                    Also: promo cards.
+                    There's no way to keep track of everything, , apparently.
+                </i>
+            </CustomImage>
+            {splitText(`
+            Up until now I treated this initiative as a single set, but they are actually <i>three</i>:
+            <ol>
+                <li><a href="https://gatherer.wizards.com/sets/FIN">Magic: The Gathering—FINAL FANTASY</a></li>
+                <li><a href="https://gatherer.wizards.com/sets/FIC">Magic: The Gathering—FINAL FANTASY Commander set</a></li>
+                <li><a href="https://gatherer.wizards.com/sets/FCA">FINAL FANTASY Through the Ages</a></li>
+            </o>
+
+            If you ever find yourself wondering, you can quickly discover which set a card belongs to by looking at the bottom left corner of the card, where you'll find the set code (FIN, FIC or FCA) and the collector number.
+            You'll also find what game the card references, as well as rarity, language etc.
+
+            As a quick rule of thumb: the "proper" set that feeds card to the current standard format is the first one.
+
+            The second set is a container for four different Commander preconstructed decks, each one themed around a single game (Final Fantasy 6, Final Fantasy 7, Final Fantasy 10 and Final Fantasy 14).
+            Cards from this set are <b>not</b> legal in standard, but can be used in Commander and other eternal formats.
+
+            Through the Ages is a "bonus sheet": they are not original cards, but reprints of older cards with a new name and art -just another way to <s>milk collectors</s> <i>give a nod to the franchise's history</i>- you can find them as extras in "regular" booster packs, but their legality is tied to their respective original card.
+            You won't be able to cast Dark Ritual in Standard anytime soon, I'm sorry.
+            `)}
 
             <StyledCollapseHeader
                 open={open[1]}
@@ -77,7 +109,7 @@ function StatefulPost() {
             </StyledCollapseHeader>
 
             <Collapse in={open[1]} timeout="auto" unmountOnExit>
-                <Chapter1/>
+                <FFpage/>
             </Collapse>
         </>
     );
